8c157a8d3ca37c8c45c19b1e9e110d450f93cb76
victrosantos <victor.santos.fk@hotmail.com>
2022-08-19 15:38:25 -0300

    Feito ex1 aula 1 bl 8

---
+++ bl8/aula1/ex1.js
@@ -0,0 +1,9 @@
+const acorda = () => 'Acordando!!';
+const bora = () => 'Bora tomar café!!';
+const partiu = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func);
+
+doingThings(acorda());
+doingThings(bora());
+doingThings(partiu());
