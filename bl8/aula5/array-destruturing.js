af4e06742374fb1094f957be01cf4af56987f9e9
victrosantos <victor.santos.fk@hotmail.com>
2022-08-25 15:30:16 -0300

    Feito outro do mesmo commit passado

--- bl8/aula5/array-destruturing.js
+++ bl8/aula5/array-destruturing.js
@@ -7,3 +7,16 @@ let bebida = 'arroz';
 console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['qwer', 'eryt', 'yuio'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['asdf', 'dfgh', 'hjkl'];
+
+const fruitSalad = (fruit, additional) => {
+  return [...fruit, ...additional]
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
