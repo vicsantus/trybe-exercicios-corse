5179771c48bbd5eeeeeb121b5cf5f8c4579cc54c
victrosantos <victor.santos.fk@hotmail.com>
2022-10-14 15:30:26 -0300

    [Test] :git: Feito alteração dos teste

--- modulo-frontend/secao6/dia1/testes-react/src/App.test.js
+++ modulo-frontend/secao6/dia1/testes-react/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/algo que não aparece/i);
   expect(linkElement).toBeInTheDocument();
 });
