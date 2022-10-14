ddcd6b41b2857347897e74c9bf4c12c8db652fb2
victrosantos <victor.santos.fk@hotmail.com>
2022-10-14 16:05:19 -0300

    [Finished] :git: Feito exercicio aula secao4 dia1

--- modulo-frontend/secao6/dia1/testes-react/src/App.test.js
+++ modulo-frontend/secao6/dia1/testes-react/src/App.test.js
@@ -2,8 +2,23 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verificando se existe o campo Email.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/algo que não aparece/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+test('Verificando se existem dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
 });
