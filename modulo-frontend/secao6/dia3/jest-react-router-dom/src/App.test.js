e79ae1d685ed7637aba54eeb0f5413a66a264f95
victrosantos <victor.santos.fk@hotmail.com>
2022-10-18 15:36:13 -0300

    Feito alguns testes de aula

--- modulo-frontend/secao6/dia3/jest-react-router-dom/src/App.test.js
+++ modulo-frontend/secao6/dia3/jest-react-router-dom/src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 import App from './App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('deve renderizar o componente App', () => {
+  renderWithRouter(<App />);
+
+  const homeTitle = screen.getByRole('heading', {
+    name: 'Você está na página Início',
+  });
+  expect(homeTitle).toBeInTheDocument();
+});
+
+it('deve renderizar o componente Sobre', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+
+  const aboutTitle = screen.getByRole('heading',
+    { name: 'Você está na página Sobre' });
+  expect(aboutTitle).toBeInTheDocument();
 });
