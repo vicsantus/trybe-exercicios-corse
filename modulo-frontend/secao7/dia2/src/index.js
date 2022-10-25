6db7e70d878d3d866691db4b097348301997a996
victrosantos <victor.santos.fk@hotmail.com>
2022-10-25 14:51:37 -0300

    Configurado redux react-redux

--- modulo-frontend/secao7/dia2/src/index.js
+++ modulo-frontend/secao7/dia2/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+const INITIAL_STATE = { count: 0 };
+const reducer = (state = INITIAL_STATE, _action) => state;
+/* eslint-disable no-underscore-dangle */
+  const store = createStore(
+   reducer, /* preloadedState, */
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+/* eslint-enable */
+
+export default store;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
