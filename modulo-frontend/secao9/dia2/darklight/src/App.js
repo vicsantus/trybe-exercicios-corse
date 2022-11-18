4448e7968ef0ecfafe8ac7c1017d4cb2909a8625
victrosantos <victor.santos.fk@hotmail.com>
2022-11-18 15:48:36 -0300

    Feito ex secao 9 dia 2

--- modulo-frontend/secao9/dia2/darklight/src/App.js
+++ modulo-frontend/secao9/dia2/darklight/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Image from './components/Image';
+import ThemeProvider from './context/ThemeProvider';
+import './style.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Header />
+      <Image />
+      <Footer />
+    </ThemeProvider>
   );
 }
-
-export default App;
