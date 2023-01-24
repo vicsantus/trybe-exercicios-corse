b6af269d8a3486ab37947bc2f24e3c2e634f71ee
victrosantos <victor.santos.fk@hotmail.com>
2023-01-24 18:21:24 -0300

    .

---
+++ modulo-backend/secao3/dia2/JOIN.sql
@@ -0,0 +1,57 @@
+
+--  Inner join
+SELECT f.actor_id, COUNT(a.actor_id), a.first_name
+FROM sakila.actor a
+INNER JOIN sakila.film_actor f
+ON a.actor_id = f.actor_id
+GROUP BY f.actor_id
+ORDER BY f.actor_id;
+
+--  Left join
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+LEFT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+RIGHT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+WHERE c.last_name IS NOT NULL
+ORDER BY c.last_name DESC;
+
+
+--  Inner join
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+INNER JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+--  Self join
+SELECT
+    CONCAT(Employee.first_name, " ", Employee.last_name) AS "Nome da Pessoa Colaboradora",
+    CONCAT(Manager.first_name, " ", Manager.last_name) AS "Nome Gerente"
+FROM
+    employees AS Employee
+INNER JOIN
+    employees AS Manager ON Employee.manager_id = Manager.employee_id;
