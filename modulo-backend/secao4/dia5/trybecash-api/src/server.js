fd907abf3267e40bba94e077c40b190ff6ca4934
victrosantos <victor.santos.fk@hotmail.com>
2023-02-03 16:59:49 -0300

    Exercicio dia 5 / Seção 4 / BackEnd

--- modulo-backend/secao4/dia5/trybecash-api/src/server.js
+++ modulo-backend/secao4/dia5/trybecash-api/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require('./app');
 // const connection = require('./db/connection');
 
@@ -5,6 +6,7 @@ const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 
   // O código abaixo é para testarmos a comunicação com o MySQL
   // const [result] = await connection.execute('SELECT 1');
