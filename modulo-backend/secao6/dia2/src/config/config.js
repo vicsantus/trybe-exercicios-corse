30393881ddd1fccddfee1dc8635c8b6a61d31caa
victrosantos <victor.santos.fk@hotmail.com>
2023-02-28 16:48:53 -0300

    Feito ORM com forengkey 1:1 1:n

---
+++ modulo-backend/secao6/dia2/src/config/config.js
@@ -0,0 +1,26 @@
+// src/config/config.js
+
+require('dotenv').config();
+
+const config = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: {
+    ...config,
+    database: 'employees_associations_development',
+  },
+  test: {
+    ...config,
+    database: 'employees_associations_test',
+    logging: false,
+  },
+  production: {
+    ...config,
+    database: 'employees_associations_production',
+  },
+};
